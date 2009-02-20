36f660a074e7a195eb60dcd037546751606812a4
Otto Hammersmith <otto.hammersmith@gmail.com>
2009-02-19 23:52:04 -0500

    added a pending spec for the users model for others to implement.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,14 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
   before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :role => "value for role"
-    }
+    @valid_attributes = Factory.attributes(:user)
   end
 
   it "should create a new instance given valid attributes" do
     User.create!(@valid_attributes)
+  end
+  
+  it "should only accept User, Developer or Customer for role" do
+    pending
   end
 end
