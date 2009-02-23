884b9e137de81111782be7c5bdb34b85dae2303b
Otto Hammersmith <otto.hammersmith@gmail.com>
2009-02-23 14:39:53 -0500

    fixing a broken spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe User do
   before(:each) do
-    @valid_attributes = Factory.attributes(:user)
+    @valid_attributes = Factory.attributes_for(:user)
   end
 
   it "should create a new instance given valid attributes" do
